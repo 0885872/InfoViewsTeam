e2d932076348bda7838754b82a7ab6043496bf35
0885872 <0885872@hr.nl>
2018-06-06 14:56:12 +0200

    Part of database code dissappeared?

--- Reserveer/Controllers/Database.cs
+++ Reserveer/Controllers/Database.cs
@@ -9,87 +9,89 @@ using Reserveer.Models;
 
 namespace Reserveer.Controllers
 {
-  public class Database
-  {
-    public List<string[]> getReservations()
+    public class Database
     {
-      List<string[]> reservations = new List<string[]>();
-      String connString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
-      using (MySqlConnection connMysql = new MySqlConnection(connString))
-      {
-        using (MySqlCommand cmdd = connMysql.CreateCommand())
+        public List<string[]> getReservations()
         {
-          cmdd.CommandText = "SELECT * FROM reservations";
-          cmdd.CommandType = System.Data.CommandType.Text;
-
-          cmdd.Connection = connMysql;
-
-          connMysql.Open();
-
-          using (MySqlDataReader reader = cmdd.ExecuteReader())
-          {
-            while (reader.Read())
+            List<string[]> reservations = new List<string[]>();
+            String connString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
+            using (MySqlConnection connMysql = new MySqlConnection(connString))
             {
-              string[] res = new string[5];
-              res[0] = reader["reservation_id"].ToString();
-              res[1] = reader["room_id"].ToString();
-              res[2] = reader["start"].ToString();
-              res[3] = reader["end"].ToString();
-              res[4] = reader["valid"].ToString();
-              res[2] = Convert.ToDateTime(res[2]).ToString("yyyy/MM/dd HH:mm");
-              res[3] = Convert.ToDateTime(res[3]).ToString("yyyy/MM/dd HH:mm");
-              reservations.Add(res);
+                using (MySqlCommand cmdd = connMysql.CreateCommand())
+                {
+                    cmdd.CommandText = "SELECT * FROM reservations";
+                    cmdd.CommandType = System.Data.CommandType.Text;
+
+                    cmdd.Connection = connMysql;
+
+                    connMysql.Open();
+
+                    using (MySqlDataReader reader = cmdd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string[] res = new string[5];
+                            res[0] = reader["reservation_id"].ToString();
+                            res[1] = reader["room_id"].ToString();
+                            res[2] = reader["start"].ToString(); 
+                            res[3] = reader["end"].ToString();
+                            res[4] = reader["valid"].ToString();
+                            res[2] = Convert.ToDateTime(res[2]).ToString("yyyy/MM/dd HH:mm");
+                            res[3] = Convert.ToDateTime(res[3]).ToString("yyyy/MM/dd HH:mm");
+                            reservations.Add(res);
+                        }
+                    }
+                }
+                connMysql.Close();
+                return reservations;
             }
-          }
         }
-        connMysql.Close();
-        return reservations;
-      }
-    }
 
-    public void setReservations(ReservationModel reservation)
-    {
-      using (MySqlConnection conn = new MySqlConnection())
-      {
-        DateTime dateTime = DateTime.Now;
-        string localDate = dateTime.ToString("yyyy/MM/dd HH:mm");
-        conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
-        conn.Open();
-        string sql = "INSERT INTO reservations (room_id,start,end,reservation_date,valid) VALUES (1,'" + reservation.start + "','" + reservation.end + "','" + localDate + "', 1);";
-        MySqlCommand command = new MySqlCommand(sql, conn);
-        command.ExecuteNonQuery();
-        conn.Close();
-      }
-    }
-
-    public string[] FindDuplicates(UserRegistration user)
-    {
-      string[] result = new string[1];
-      String connString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
-      using (MySqlConnection connMysql = new MySqlConnection(connString))
-      {
-        using (MySqlCommand cmdd = connMysql.CreateCommand())
+        public void setReservations(ReservationModel reservation)
         {
-          
-          cmdd.CommandText = "SELECT COUNT(user_mail) AS res FROM user WHERE user_mail = '" + user.Mail + "';";
-          cmdd.CommandType = System.Data.CommandType.Text;
-
-          cmdd.Connection = connMysql;
-
-          connMysql.Open();
-
-          using (MySqlDataReader reader = cmdd.ExecuteReader())
-          {
-            while (reader.Read())
+            using (MySqlConnection conn = new MySqlConnection())
             {
-              
-              result[0] = reader["res"].ToString();
-              
+                DateTime dateTime = DateTime.Now;
+                string localDate = dateTime.ToString("yyyy/MM/dd HH:mm");
+                conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
+                conn.Open();
+                string sql = "INSERT INTO reservations (room_id,start,end,reservation_date,valid) VALUES (1,'" + reservation.start + "','" + reservation.end + "','" + localDate + "', 1);";
+                MySqlCommand command = new MySqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
             }
-          }
         }
-      }
-      return result;
+
+        public string[] FindDuplicates(UserRegistration user)
+        {
+            string[] result = new string[1];
+            String connString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
+            using (MySqlConnection connMysql = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmdd = connMysql.CreateCommand())
+                {
+
+                    cmdd.CommandText = "SELECT COUNT(user_mail) AS res FROM user WHERE user_mail = '" + user.Mail + "';";
+                    cmdd.CommandType = System.Data.CommandType.Text;
+
+                    cmdd.Connection = connMysql;
+
+                    connMysql.Open();
+
+                    using (MySqlDataReader reader = cmdd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            result[0] = reader["res"].ToString();
+
+                        }
+                    }
+                }
+            }
+            return result;
+        }
     }
-  }
+}
+    }
 }
