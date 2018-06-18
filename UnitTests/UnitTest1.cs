f857859f3e0217b8a8a7f08ce0d760fdd71aa711
tvangeest <0919804@hr.nl>
2018-06-18 15:47:22 +0200

    Added: kind of mock unittests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,124 @@
+using System;
+using Xunit;
+using Reserveer;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+    public Reserveer.Controllers.Database db;
+
+    public UnitTest1()
+    {
+      Reserveer.Controllers.Database database = new Reserveer.Controllers.Database();
+      db = database;
+    }
+        [Fact]
+        public void Test1()
+        {
+      
+          string result = db.getDomainCheck("hr.nl");
+          Assert.Equal(result, "1");
+
+        }
+
+      [Fact]
+      public void Test2()
+    {
+      List<string[]> result = db.getGroupAdmin();
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test3()
+    {
+      List<string[]> result = db.getGroupRoomReservation();
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test4()
+    {
+      List<string[]> result = db.getGroupRooms();
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test5()
+    {
+      List<string[]> result = db.getGroupUser();
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test6()
+    {
+      string[] result = db.getLatestTemperature("1");
+      Assert.Equal(result, null);
+
+
+
+    }
+    [Fact]
+    public void Test7()
+    {
+      List<string[]> result = db.getReservations("1");
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test8()
+    {
+      string result = db.getRoomName("1");
+      Assert.Equal(result, null);
+
+    }
+    [Fact]
+    public void Test9()
+    {
+      List<string[]> result = db.getRoomReservation();
+      Assert.Equal(result, null);
+
+    }
+
+    [Fact]
+    public void Test10()
+    {
+      List<string[]> result = db.getRoomSensors("1");
+      Assert.Equal(result, null);
+
+    }
+
+    [Fact]
+    public void Test11()
+    {
+      List<string[]> result = db.getUserGroup();
+      Assert.Equal(result, null);
+
+    }
+
+    [Fact]
+    public void Test12()
+    {
+      string result = db.getUserMail("42");
+      Assert.Equal(result, "nee@Hotmail.com");
+
+
+    }
+
+    //[Fact]
+    //public void Test13()
+    //{
+    //  db.setReservations();
+
+    //}
+
+    //[Fact]
+    //public void Test14()
+    //{
+    //  db.VerifyMail();
+
+    //}      
+  }
+}
