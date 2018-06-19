4697b1fb81e7adccf46a619d6f696122ab805510
0885872 <0885872@hr.nl>
2018-06-19 15:20:02 +0200

    Raspberry scheduler, deleted unnes. code

--- UnitTests/ScheduleTests.cs
+++ UnitTests/ScheduleTests.cs
@@ -8,26 +8,19 @@ namespace UnitTests
 {
     public class ScheduleTests
     {
-    public Reserveer.Controllers.ScheduleController sd;
+        public Reserveer.Controllers.ScheduleController sd;
 
-    public ScheduleTests()
-    {
-      Reserveer.Controllers.ScheduleController schedule = new Reserveer.Controllers.ScheduleController();
-      sd = schedule;
-    }
+        public ScheduleTests()
+        {
+            Reserveer.Controllers.ScheduleController schedule = new Reserveer.Controllers.ScheduleController();
+            sd = schedule;
+        }
         [Fact]
         public void Index_ReturnsViews_NotNull()
         {
-      var returnedView = sd.Index() as ViewResult;
-      Assert.NotNull(returnedView.ViewData["results"]);
-      Assert.NotNull(returnedView.ViewData["NumTimes"]);
+            var returnedView = sd.Index() as ViewResult;
+            Assert.NotNull(returnedView.ViewData["results"]);
+            Assert.NotNull(returnedView.ViewData["NumTimes"]);
+        }
     }
-
-      [Fact]
-      public void SetReservation_etc_etc()
-    {
-
-
-    }
-  }
 }
