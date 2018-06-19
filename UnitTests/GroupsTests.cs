e2a96850ca4f2d1af7b2d02d396682f1b6bf1fd3
0885872 <0885872@hr.nl>
2018-06-19 13:13:59 +0200

    GroupsController unittests fixed

--- UnitTests/GroupsTests.cs
+++ UnitTests/GroupsTests.cs
@@ -2,100 +2,37 @@ using System;
 using Xunit;
 using Reserveer;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests
 {
     public class GroupsTests
     {
-    public Reserveer.Controllers.GroupsController grp;
+        public Reserveer.Controllers.GroupsController grp;
 
-    public GroupsTests()
-    {
-      Reserveer.Controllers.GroupsController group = new Reserveer.Controllers.GroupsController();
-      grp = group;
-    }
+        public GroupsTests()
+        {
+            Reserveer.Controllers.GroupsController group = new Reserveer.Controllers.GroupsController();
+            grp = group;
+        }
         [Fact]
         public void Test1()
         {
+            var returnedView = grp.Index() as ViewResult;
+            Assert.NotNull(returnedView.ViewData["results"]);
+            Assert.NotNull(returnedView.ViewData["NumTimes"]);
 
         }
 
-      [Fact]
-      public void Test2()
-    {
+        [Fact]
+        public void Test2()
+        {
+            var returnedView = grp.Rooms("TestNameUser") as ViewResult;
+            Assert.NotNull(returnedView);
+            Assert.NotNull(returnedView.ViewData["results"]);
+            Assert.NotNull(returnedView.ViewData["Message"]);
+            Assert.NotNull(returnedView.ViewData["NumTimes"]);
 
-
+        }
     }
-    [Fact]
-    public void Test3()
-    {
-
-
-    }
-    [Fact]
-    public void Test4()
-    {
-
-
-    }
-    [Fact]
-    public void Test5()
-    {
-
-
-    }
-    [Fact]
-    public void Test6()
-    {
-
-    }
-    [Fact]
-    public void Test7()
-    {
-
-    }
-    [Fact]
-    public void Test8()
-    {
-
-
-    }
-    [Fact]
-    public void Test9()
-    {
-
-    }
-
-    [Fact]
-    public void Test10()
-    {
-
-    }
-
-    [Fact]
-    public void Test11()
-    {
-
-    }
-
-    [Fact]
-    public void Test12()
-    {
-
-    }
-
-    //[Fact]
-    //public void Test13()
-    //{
-    //  db.setReservations();
-
-    //}
-
-    //[Fact]
-    //public void Test14()
-    //{
-    //  db.VerifyMail();
-
-    //}      
-  }
 }
