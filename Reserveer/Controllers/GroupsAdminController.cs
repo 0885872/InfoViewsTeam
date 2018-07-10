42ceee58bd449550a12357c7015c408c56db073e
tvangeest <0919804@hr.nl>
2018-07-10 19:01:01 +0200

    Commenting for GroupsAdmin views and GroupsAdmin Controller

--- Reserveer/Controllers/GroupsAdminController.cs
+++ Reserveer/Controllers/GroupsAdminController.cs
@@ -50,14 +50,17 @@ namespace Reserveer.Controllers
                 try
                 {
                     Database db = new Database();
+                    // Creates string lists for all the information that gets taken from the database files
                     List<string[]> results = db.getGroupAdmin();
                     List<string[]> results2 = db.getGroupRooms();
                     List<string[]> results3 = db.getGroupUser();
                     List<string[]> results4 = db.getGroupRoomReservation();
+                    // Converts these string list to json results
                     var json = JsonConvert.SerializeObject(results);
                     var json2 = JsonConvert.SerializeObject(results2);
                     var json3 = JsonConvert.SerializeObject(results3);
                     var json4 = JsonConvert.SerializeObject(results4);
+                    // Views all the data taken from the jsons
                     ViewData["results"] = json;
                     ViewData["results2"] = json2;
                     ViewData["results3"] = json3;
@@ -67,6 +70,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("Profile Exception: {0}", e);
+                    // Redirects to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -107,6 +111,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("Roomprofile Exception: {0}", e);
+                    // Redirect to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -123,17 +128,20 @@ namespace Reserveer.Controllers
                     {
                         conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
                         conn.Open();
+                        // Create sql string to Update the room with the filled in information
                         String sql =
                             "UPDATE rooms SET rooms.room_name = '" + info.RoomName + "', rooms.room_number = " + info.RoomNumber + ",  rooms.room_floor = " + info.RoomFloor + ", rooms.room_facilities = '" + info.RoomFacility + "', rooms.room_comment = '" + info.RoomComment + "' WHERE rooms.room_id = " + info.RoomID + "; ";
                         MySqlCommand command = new MySqlCommand(sql, conn);
                         command.ExecuteNonQuery();
                         conn.Close();
+                        // Redirect to the GroupsAdmin/Profile page, with the GroupID of the current user
                         return RedirectToAction("Profile", "GroupsAdmin", test.GroupID);
                     }
                 }
-                catch (Exception e)
+                catch (Exception e) // Caches any exceptions that would cause an error
                 {
                     Debug.WriteLine("UpdateRoom Exception: {0}", e);
+                    // Redirects to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -199,6 +207,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("UpdateRoomSensor Exception: {0}", e);
+                    // Redirects to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -215,6 +224,7 @@ namespace Reserveer.Controllers
                     {
                         conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
                         conn.Open();
+                        // Creates query to add all information taken from the filled in textboxes and creates a new entry in the 'rooms' table in database
                         String sql =
                             "INSERT INTO rooms(room_name, room_number, group_id, room_floor, room_facilities,  available, room_comment) VALUES ('" + info.RoomName + "', " + info.RoomNumber + ", 1, " + info.RoomFloor + ", '" + info.RoomFacility + "', 0, '" + info.RoomComment + "'); ";
                         MySqlCommand command = new MySqlCommand(sql, conn);
@@ -226,6 +236,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("AddRoomInfo Exception: {0}", e);
+                    // Redirects to error page
                     return RedirectToAction("Error","Home");
                     throw;
                 }}
@@ -242,17 +253,20 @@ namespace Reserveer.Controllers
                     {
                         conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
                         conn.Open();
+                        // Creates query to Update the Group name into the specified GroupName in the input field
                         String sql =
                             "UPDATE `group` SET group_name = '" + info.GroupName + "' WHERE group_id = '" + info.GroupID + "';";
                         MySqlCommand command = new MySqlCommand(sql, conn);
                         command.ExecuteNonQuery();
                         conn.Close();
+                        // Redirects to GroupsAdmin/Profile with the correct GroupID
                         return RedirectToAction("Profile", "GroupsAdmin", info.GroupID);
                     }
                 }
                 catch (Exception e)
                 {
                     Debug.WriteLine("UpdateGroupName Exception: {0}", e);
+                    // Redirects to the error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -267,8 +281,8 @@ namespace Reserveer.Controllers
                     using (MySqlConnection conn = new MySqlConnection())
                     {
                         conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
-                        //string test123 = "1";
                         conn.Open();
+                        // Creates query to deactivate a user
                         String sql =
                             "UPDATE user SET user.active = '1' WHERE user.user_id = '" + userId + "';";
                         MySqlCommand command = new MySqlCommand(sql, conn);
@@ -280,6 +294,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("DeactivateUser Exception", e);
+                    // Redirects to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
@@ -294,8 +309,8 @@ namespace Reserveer.Controllers
                     using (MySqlConnection conn = new MySqlConnection())
                     {
                         conn.ConnectionString = "Server=drakonit.nl;Database=timbrrf252_roomreserve;Uid=timbrrf252_ictlab;Password=ictlabhro;SslMode=none";
-                        //string test123 = "1";
                         conn.Open();
+                        // Creates a query to set the specified reservation to invalid
                         String sql =
                             "UPDATE reservations SET reservations.valid = '0'  WHERE reservations.reservation_id = " + reservationId + ";";
                         MySqlCommand command = new MySqlCommand(sql, conn);
@@ -307,6 +322,7 @@ namespace Reserveer.Controllers
                 catch (Exception e)
                 {
                     Debug.WriteLine("DeleteReservation Exception", e);
+                    // Redirects to error page
                     return RedirectToAction("Error", "Home");
                     throw;
                 }}
