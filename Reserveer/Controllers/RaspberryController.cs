3e449c5450b5f5eed6ba743753c9d7e43c3af275
0885872 <0885872@hr.nl>
2018-07-11 12:25:04 +0200

    Commented RaspberryController

--- Reserveer/Controllers/RaspberryController.cs
+++ Reserveer/Controllers/RaspberryController.cs
@@ -18,43 +18,44 @@ namespace Reserveer.Controllers
         [HttpGet]
         public ActionResult Index()
         {
+            //Called when Raspberry route requested
             {
                 try
                 {
-                    string room = Request.Query["roomid"];
+                    string room = Request.Query["roomid"]; //Get roomid from url parameter
 
                     //QRCode Code nog commenten
                     var request = HttpContext.Request;
-                    using (MemoryStream ms = new MemoryStream())
+                    using (MemoryStream ms = new MemoryStream()) //setup qr code generator
                     {
                         QRCodeGenerator qrGenerator = new QRCodeGenerator();
 
-                        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://145.24.222.130/Schedule?RoomId=" + room, QRCodeGenerator.ECCLevel.Q);
-                        QRCode qrCode = new QRCode(qrCodeData);
+                        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://145.24.222.130/Schedule?RoomId=" + room, QRCodeGenerator.ECCLevel.Q); //Data for qr code used to generate qr code.
+                        QRCode qrCode = new QRCode(qrCodeData); 
 
 
-                        using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+                        using (Bitmap qrCodeImage = qrCode.GetGraphic(20))//Generate QR code
                         {
                             qrCodeImage.Save(ms, ImageFormat.Png);
                             ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
                         }
                     }
 
-                    Database db = new Database();
-                    List<string[]> results = db.getReservations(room);
-                    var json = JsonConvert.SerializeObject(results);
-                    ViewData["results"] = json;
+                    Database db = new Database(); //Make new instance of database class
+                    List<string[]> results = db.getReservations(room); //Calls getReservations method of database class and stores returned values as list of string []'s
+                    var json = JsonConvert.SerializeObject(results); //Converts prevoius list of string []'s to json object
+                    ViewData["results"] = json; //Hands over json object to view
 
-                    string[] temperatureData = db.getLatestTemperature(room);
-                    var jsonTemp = JsonConvert.SerializeObject(temperatureData);
-                    ViewData["temp"] = jsonTemp;
-                    return View();
+                    string[] temperatureData = db.getLatestTemperature(room); //Calls getLatestTemperature method of database class and stores returned values in string []
+                    var jsonTemp = JsonConvert.SerializeObject(temperatureData); //Converts prevoius string [] to json object
+                    ViewData["temp"] = jsonTemp; //Hands over json object to view
+                    return View(); //Returns view
 
                 }
-                catch (Exception e)
+                catch (Exception e) //Exception catcher
                 {
                     Debug.WriteLine("Index raspberry Exception: {0}", e);
-                    return RedirectToAction("Error", "Home");
+                    return RedirectToAction("Error", "Home"); //Shows error page
                     throw;
                 }}
         }
